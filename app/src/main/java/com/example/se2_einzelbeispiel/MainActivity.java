aad21c3f20539e2600d25f9cf94bbf70dc31a478
Nejc Dolinšek <nejc.dolinsek45@gmail.com>
2021-03-07 20:35:31 +0100

    Implemented the TCP Client and made some changes in the UI design

--- app/src/main/java/com/example/se2_einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/se2_einzelbeispiel/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.se2_einzelbeispiel;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,8 +10,39 @@ import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.*;
+import java.net.*;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    public void run() {
+        TextView TextAnswer = (TextView) findViewById(R.id.Answer);
+        EditText InputText = (EditText) findViewById(R.id.Input);
+
+        try {
+            String serverInput = InputText.getText().toString();
+
+            Socket clientSocked = new Socket("se2-isys.aau.at", 53212);
+
+            DataOutputStream outToServer = new DataOutputStream(clientSocked.getOutputStream());
+
+            BufferedReader inFormServer = new BufferedReader(new InputStreamReader(clientSocked.getInputStream()));
+
+            outToServer.writeBytes(serverInput);
+
+            TextAnswer.setText(inFormServer.readLine());
+
+            clientSocked.close();
+        }catch (Exception e){
+            TextAnswer.setText("Something went wrong");
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +51,12 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        final Button matNrToServer = (Button) findViewById(R.id.matToServerBTN);
+        matNrToServer.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                run();
             }
         });
     }
